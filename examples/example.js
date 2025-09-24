4e349dabbf1142a9883fcf268560bf65c393557d
Andrea Bianchi <andrea@kaist.ac.kr>
2025-09-24 21:54:02 +0900

    New version

--- examples/example.js
+++ examples/example.js
@@ -1,52 +1,27 @@
-# make::lab csv utilities
-
-Make it easy to parse csv files, extract columns and rows, and transfor the data to numbers/strings/booleans.
-
-## Usage
-
-### Parse the CSV file
-
-You can extract columns or rows.
-
-```js
 import {
   parseFile,
   getColumnByName,
   getRowByIndex,
   asRows,
   asColumns,
+  choose,
+  asNumbers,
+  header,
+  values,
+  asBooleans,
 } from '@makelab/csv';
 
 // Rows
 const cols = parseFile('example_data.csv').then(asColumns);
 
-const colA = cols.then(getColumnByName('A'));
-
-const row0 = await parseFile('example_data.csv')
-  .then(asRows)
-  .then(getRowByIndex(0));
-```
-
-### Transforming data
-
-You can now convert the data in the columns/rows to number, strings (default) or booleans
-
-```js
-import { choose, asNumbers, header, values, asBooleans } from '@makelab/csv';
-
 const colA = await cols.then(getColumnByName('A')).then(asNumbers).then(choose);
+
 const colB = await cols.then(getColumnByName('B')).then(choose);
 const colC = await cols
   .then(getColumnByName('C'))
   .then(asBooleans)
   .then(choose);
-```
 
-### Extract data
-
-You can extract headers and values from rows and co,umns
-
-```js
 console.log('--- COLUMNS ---');
 console.log(`Column ${colA.header} (as numbers):`, values(colA));
 console.log(`Column ${colB.header} (as strings):`, values(colB));
@@ -61,12 +36,3 @@ await parseFile('example_data.csv')
   .then((s) => s.join(', ')) // join as string
   .then((s) => `All headers: ${s}`) // format
   .then(console.log);
-```
-
-## Example
-
-See a [complete example](./examples/example.js) here.
-
-## Credits
-
-Made with ♥️ by [MAKinteract](https://make.kaist.ac.kr)
--- main.ts
+++ main.ts
@@ -2,56 +2,44 @@
 import { parse } from 'jsr:@std/csv@1.0.6';
 import { z } from 'npm:/zod@4.1.11';
 
-export type Data = string[][];
+type Data = string[][];
 
-export type Value = string | number | boolean;
+type Value = string | number | boolean;
 
-export type Row = {
+type Row = {
   header: string[];
   values: Value[];
 };
 
-export type Column = {
+type Column = {
   header: string;
   values: Value[];
 };
 
-export type RowCol = Row | Column;
+type RowCol = Row | Column;
 
-export async function getData(filename: string): Promise<Data> {
+// Parse
+export async function parseFile(
+  filename: string,
+  skipFirstRow: boolean = false,
+  separator: string = ','
+): Promise<Data> {
   const text = await Deno.readTextFile(filename);
 
   return parse(text, {
-    skipFirstRow: false,
+    skipFirstRow,
     strip: true,
-  });
+    separator,
+  }) as Data;
 }
 
-export function rows(data: Data): Row[] {
+export function asRows(data: Data): Row[] {
   const [header, ...values] = data;
   const r = values.map((v) => ({ header, values: v }));
   return r;
 }
 
-export function columns(data: Data): Column[] {
-  const [first, ...values] = data;
-  return first.map((name, i) => ({
-    header: name,
-    values: values.map((v) => v[i]),
-  }));
-}
-
-export const columnByName =
-  (name: string) =>
-  (columns: Column[]): Column => {
-    const res = columns.find((c) => c.header === name);
-    if (!res) {
-      throw new Error(`Column ${name} not found`);
-    }
-    return res;
-  };
-
-export const rowByIndex =
+export const getRowByIndex =
   (index: number) =>
   (rows: Row[]): Row => {
     const res = rows[index];
@@ -61,6 +49,25 @@ export const rowByIndex =
     return res;
   };
 
+export function asColumns(data: Data): Column[] {
+  const [first, ...values] = data;
+  return first.map((name, i) => ({
+    header: name,
+    values: values.map((v) => v[i]),
+  }));
+}
+
+export const getColumnByName =
+  (name: string) =>
+  (columns: Column[]): Column => {
+    const res = columns.find((c) => c.header === name);
+    if (!res) {
+      throw new Error(`Column ${name} not found`);
+    }
+    return res;
+  };
+
+// Transformations
 export const floats: z.ZodTransform<number, string> = z.transform(parseFloat);
 export const integers: z.ZodTransform<number, string> = z.transform(
   (str: string) => parseInt(str, 10)
@@ -76,6 +83,18 @@ export const transform =
       values: z.string().pipe(schema).array().parse(data.values),
     };
   };
+
+export function asStrings(data: RowCol): RowCol {
+  return transform(strings)(data);
+}
+
+export function asNumbers(data: RowCol): RowCol {
+  return transform(floats)(data);
+}
+
+export function asBooleans(data: RowCol): RowCol {
+  return transform(booleans)(data);
+}
 
 export const choose = (data: RowCol): RowCol => {
   return {
